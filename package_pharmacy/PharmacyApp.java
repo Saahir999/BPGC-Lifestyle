b498ef348728b1998f2d15b290614368257d5cb7
Saahir <saahirvaidya@gmail.com>
2022-11-23 13:29:19 +0530

    Created Pharmacy package

---
+++ package_pharmacy/PharmacyApp.java
@@ -0,0 +1,8 @@
+package package_pharmacy;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class PharmacyApp{
+    HashMap<String,ArrayList> data = new HashMap<String,ArrayList>();
+}
---
+++ package_pharmacy/PharmacyRead.java
@@ -0,0 +1,5 @@
+package package_pharmacy;
+
+class PharmacyRead{
+    
+}
